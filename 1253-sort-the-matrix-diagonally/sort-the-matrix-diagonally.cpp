94c06e941517033a7747b17fbcd3208513eeee85
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-01-27 10:29:14 +0530

    Time: 2 ms (62.71%) | Memory: 14.3 MB (5.57%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -3,48 +3,29 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) 
     {
         int rows = mat.size(), cols = mat[0].size();
+        unordered_map<int, vector<int>> mp;
 
         for(int i=0; i<rows; i++)
         {
-            int currR = i;
-            int currC = 0;
-            vector<int> v;
-            while(currR < rows && currC < cols)
+            for(int j=0; j<cols; j++)
             {
-                v.push_back(mat[currR][currC]);
-                currR++; currC++;
-            }
-
-            sort(v.begin(), v.end());
-            currR = i; currC = 0;
-            int idx = 0;
-
-            while(currR < rows && currC < cols)
-            {
-                mat[currR][currC] = v[idx];
-                currR++; currC++; idx++;
+                mp[i-j].push_back(mat[i][j]);
             }
         }
 
-        for(int i=1; i<cols; i++)
+        for(auto a:mp ) 
         {
-            int currC = i;
-            int currR = 0;
-            vector<int> v;
-            while(currR < rows && currC < cols)
-            {
-                v.push_back(mat[currR][currC]);
-                currR++; currC++;
-            }
-
+            vector<int> v = a.second;
             sort(v.begin(), v.end());
-            currC = i; currR = 0;
-            int idx = 0;
+            mp[a.first] = v;
+        }
 
-            while(currR < rows && currC < cols)
+        for(int i=rows-1; i>=0; i--)
+        {
+            for(int j=cols-1; j>=0; j--)
             {
-                mat[currR][currC] = v[idx];
-                currR++; currC++; idx++;
+                mat[i][j] = mp[i-j].back();
+                mp[i-j].pop_back();
             }
         }
 
