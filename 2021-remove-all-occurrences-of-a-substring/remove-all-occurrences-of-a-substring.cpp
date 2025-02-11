a636def46dc483485fbb5c5a976740cbb99d687e
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-02-11 08:01:57 +0530

    Time: 2 ms (40.90%) | Memory: 9.3 MB (68.15%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        //for solution watch supreme batch strings lecture 2
-        //or it is saved in kanhaiyamac as removeOccurrrences.cpp
-    if(s.find(part) == string::npos) {
-        return s;
-    }
-    int fount = s.find(part);
-    s.erase(fount, part.length());
+    string removeOccurrences(string s, string part) 
+    {
+        int n = s.length(), partN = part.length()-1;
+        string ans = "";
+        for(int i=0; i<n; i++)
+        {
+            ans.push_back(s[i]);
+            if(s[i] == part[partN])
+            {
+                bool found = true;
+                int e = partN;
+                for(int j=ans.length()-1; j>=0; j--)
+                {
+                    if(e < 0) break;
+                    if(ans[j] != part[e]) 
+                    {
+                        found = false;
+                        break;
+                    }
+                    e--;
+                }
+                if(found && ans.length() >= part.length())
+                {
+                    for(int k=0; k<part.length(); k++)
+                        ans.pop_back();
+                }
+            }
+        }
 
-    return removeOccurrences(s, part);
+        return ans;
     }
 };
