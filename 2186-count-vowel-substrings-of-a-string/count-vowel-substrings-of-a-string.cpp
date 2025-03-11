d70cabff8bdfc32d0c94d375cfe2a31aedee2385
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-03-11 18:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (77.16%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -1,45 +1,32 @@
 class Solution {
-    bool isVowel(char ch)
-    {
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            return true;
-        return false;
-    }
 public:
     int countVowelSubstrings(string word) 
     {
-        int l = 0, r = 0, cnt = 0, n = word.size();
-        unordered_map<char, int> mp;
-
-        while(r < n)
+        int n = word.size(), cnt = 0;
+        unordered_map<char, int> freq;
+        
+        for (int l = 0, r = 0, vowelStart = 0; r < n; r++) 
         {
-            char ch = word[r];
-            if(isVowel(ch)) mp[ch]++;
-            else
-            {
-                l = r+1;
-                mp.clear();
+            if (string("aeiou").find(word[r]) == string::npos) 
+            { 
+                // Reset if a non-vowel is encountered
+                freq.clear();
+                l = r + 1;
+                vowelStart = l;
+                continue;
             }
+            
+            freq[word[r]]++;
 
-            if(mp.size() == 5)
+            // When all 5 vowels are present, start counting substrings
+            while (freq.size() == 5) 
             {
-                int idx = r;
-                int validSubstr = 0;
-                while(idx < n && isVowel(word[idx++])) validSubstr++;
-                
-                cnt += validSubstr;
-
-                while(mp.size() == 5)
-                {
-                    mp[word[l]]--;
-                    if(mp[word[l]] == 0) mp.erase(word[l]);
-                    else
-                        cnt += validSubstr;
-                    l++;
-                }
-
+                freq[word[l]]--;
+                if (freq[word[l]] == 0) freq.erase(word[l]);
+                l++;
             }
-            r++;
+            
+            cnt += l - vowelStart; // Count valid substrings
         }
         return cnt;
     }
