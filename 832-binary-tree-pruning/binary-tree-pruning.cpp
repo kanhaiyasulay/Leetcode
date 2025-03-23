bb490172e3e39a410ba454d22ef185dfc60a9b6e
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-03-23 06:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (75.13%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -1,63 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool secondLoop(TreeNode* root)
-    {
-        if(!root) return false;
-
-        if(root->val == 1) return true;
-
-        bool leftCheck = secondLoop(root->left);
-        bool rightCheck = secondLoop(root->right);
-
-        return leftCheck || rightCheck;
+    bool checkOne(TreeNode* root) {
+        if(!root)
+            return false;
+        
+        if(root->val == 1)
+            return true;
+        
+        return checkOne(root->left) || checkOne(root->right);
     }
-    void firstLoop(TreeNode* root)
-    {
-        if(!root) return;
-        if(!secondLoop(root)) 
-        {
-            root->left = NULL;
-            root->right = NULL;
-            root->val = -1;
-            return;
-        }
-
-        firstLoop(root->left);
-        firstLoop(root->right);
-
-    }
-    void deleteNodes(TreeNode* root)
-    {
-        if(!root) return;
-
-        if(root->left)
-        {
-            if(root->left->val == -1) root->left = NULL;
-        }
-        if(root->right)
-        {
-            if(root->right->val == -1) root->right = NULL;
-        }
-
-        deleteNodes(root->left);
-        deleteNodes(root->right);
-    }
-    TreeNode* pruneTree(TreeNode* root) 
-    {
-        firstLoop(root);
-        deleteNodes(root);
-        if(!root->left && !root->right && root->val == -1) return NULL;
+    
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root)
+            return NULL;
+        
+        pruneTree(root->left);
+        pruneTree(root->right);
+        
+        if(!checkOne(root->left))  root->left = NULL;
+        if(!checkOne(root->right)) root->right = NULL;
+        
+        
+        if(!root->left && !root->right && root->val == 0)
+            return NULL;
         return root;
     }
 };
