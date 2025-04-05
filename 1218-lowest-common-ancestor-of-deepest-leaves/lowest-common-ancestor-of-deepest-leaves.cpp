55a2741569f60e248d15684a6e8a4ccda57d36dd
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-04-05 07:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (24.47%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,25 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    int getDepth(TreeNode* root) {
-        if (!root)
-            return 0;
-        return max(getDepth(root->right), getDepth(root->left)) + 1;
+    unordered_set<TreeNode*> deepLeaves(TreeNode* root)
+    {
+        unordered_set<TreeNode*> st;
+        vector<TreeNode*> arr;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int size = q.size();
+
+            for(int i=0; i<size; i++)
+            {
+                TreeNode* front = q.front();
+                q.pop();
+                arr.push_back(front);
+
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+            }
+            if(q.empty()) 
+            {
+                int arr_size = arr.size();
+                for(int i=0; i<arr_size; i++)
+                {
+                    st.insert(arr.back());
+                    arr.pop_back();
+                }
+                return st;
+            }
+            else arr.clear();
+        }
+        return st;
     }
-    
-    TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        if (!root)
-            return NULL;
-        
-        int right_depth = getDepth(root->right);
-        int left_depth = getDepth(root->left);
-        
-        if (right_depth == left_depth)
+public:
+    TreeNode* LCA(TreeNode* root, unordered_set<TreeNode*>& st)
+    {
+        if(!root) return NULL;
+
+        if(st.find(root) != st.end())
+        {
             return root;
-        
-        if (right_depth > left_depth) 
-            return lcaDeepestLeaves(root->right);
-        
-        else 
-            return lcaDeepestLeaves(root->left);
+        }
+
+        TreeNode* leftCheck = LCA(root->left, st);
+        TreeNode* rightCheck = LCA(root->right, st);
+
+        if(leftCheck && rightCheck) return root;
+        if(!leftCheck) return rightCheck;
+        return leftCheck;
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) 
+    {
+        unordered_set<TreeNode*> st = deepLeaves(root);
+        TreeNode* lca = LCA(root, st);
+        return lca;
     }
 };
