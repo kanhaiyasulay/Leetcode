2a4496d98d7c42f092380ae275d0d55e17d5a592
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-04-05 23:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (90.00%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,33 +1,26 @@
 class Solution {
-    void inclusionExclusion(vector<int>& nums, vector<vector<int>>& subsets, vector<int>& temp, int idx)
+    void inclusionExclusion(vector<int>& nums, int& ans, int xorSum, int idx)
     {
         if(idx >= nums.size())
         {
-            subsets.push_back(temp);
+            ans += xorSum;
             return;
         }
 
-        temp.push_back(nums[idx]);
-        inclusionExclusion(nums, subsets, temp, idx+1);
-        temp.pop_back();
+        xorSum ^= nums[idx];
+        inclusionExclusion(nums, ans, xorSum, idx+1);
+        xorSum ^= nums[idx];
 
-        inclusionExclusion(nums, subsets, temp, idx+1);
+        inclusionExclusion(nums, ans, xorSum, idx+1);
     }
 public:
     int subsetXORSum(vector<int>& nums) 
     {
         vector<vector<int>> subsets;
-        vector<int> temp;
-        inclusionExclusion(nums, subsets, temp, 0);
-
         int ans = 0;
-        for(auto it:subsets )
-        {
-            int sum = 0;
-            for(auto a:it ) sum ^= a;
-            ans += sum;
-        }
-        
+        int xorSum = 0;
+        inclusionExclusion(nums, ans, xorSum, 0);
+
         return ans;
     }
 };
