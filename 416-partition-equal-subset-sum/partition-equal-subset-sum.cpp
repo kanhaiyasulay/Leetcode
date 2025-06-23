ae19c4238283e17078738d549be17dc088519ff5
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-06-23 16:41:18 +0530

    Time: 151 ms (34.36%) | Memory: 17.4 MB (64.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -20,7 +20,26 @@ public:
         for(auto it:nums ) total += it;
         if(total%2 != 0) return false;
 
-        vector<vector<int>> dp(n, vector<int>((total/2)+1, -1));
-        return backtrack(nums, dp, total/2, n-1);
+        int reqSum = total/2;
+
+        vector<vector<bool>> dp(n, vector<bool>(reqSum+1, false));
+        for(int i=0; i<n; i++) dp[i][0] = true;
+
+        if(nums[0] <= reqSum) dp[0][nums[0]] = true;
+
+        for(int i=1; i<n; i++)
+        {
+            for(int sum=0; sum<=reqSum; sum++)
+            {
+                bool exclusion = dp[i-1][sum];
+                bool inclusion = false;
+                if(nums[i] <= sum)
+                    inclusion = dp[i-1][sum-nums[i]];
+
+                dp[i][sum] = (inclusion || exclusion);
+            }
+        }
+
+        return dp[n-1][reqSum];
     }
 };
