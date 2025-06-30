fd68d7b7f283177fbe162231b58527ae454239fb
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-06-30 08:06:33 +0530

    Time: 15 ms (55.15%) | Memory: 19.4 MB (37.85%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int diffEqlToK(vector<int>& nums, int total, int target, int sum, int idx)
+    int diffEqlToK(vector<int>& nums, int total, int target, int sum, int idx, vector<vector<int>>& dp)
     {
         if(idx < 0)
         {
             if (2 * sum == total + target) return 1;
             return 0;
         }
+        if(dp[idx][sum] != -1) return dp[idx][sum];
 
-        int inclusion = diffEqlToK(nums, total, target, sum+nums[idx], idx-1);
-        int exclusion = diffEqlToK(nums, total, target, sum, idx-1);
+        int inclusion = diffEqlToK(nums, total, target, sum+nums[idx], idx-1, dp);
+        int exclusion = diffEqlToK(nums, total, target, sum, idx-1, dp);
 
-        return inclusion + exclusion;
+        return dp[idx][sum] = inclusion + exclusion;
     }
     int findTargetSumWays(vector<int>& nums, int target)
     {
         int total = accumulate(nums.begin(), nums.end(), 0);
         int n = nums.size();
-        return diffEqlToK(nums, total, target, 0, n-1);
+        vector<vector<int>> dp(n, vector<int>(total+1, -1));
+        return diffEqlToK(nums, total, target, 0, n-1, dp);
     }
 };
