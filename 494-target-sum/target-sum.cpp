c047fe91fe95c2a317139d4fbc74b020d1d347b2
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-07-06 10:03:58 +0530

    Time: 198 ms (32.94%) | Memory: 11.7 MB (75.66%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int diffEqlToK(vector<int>& nums, int total, int target, int sum, int idx, vector<vector<int>>& dp)
+    int backtrack(vector<int>& nums, int target, int idx)
     {
-        if(idx < 0)
+        if(idx == 0)
         {
-            if (2 * sum == total + target) return 1;
+            if(target == 0 && target == nums[0]) return 2;
+            if(target == 0) return 1;
+            if(target == nums[0]) return 1;
             return 0;
         }
-        if(dp[idx][sum] != -1) return dp[idx][sum];
-
-        int inclusion = diffEqlToK(nums, total, target, sum+nums[idx], idx-1, dp);
-        int exclusion = diffEqlToK(nums, total, target, sum, idx-1, dp);
-
-        return dp[idx][sum] = inclusion + exclusion;
+        
+        int exclusion = backtrack(nums, target, idx-1);
+        int inclusion = 0;
+        if(target >= nums[idx])
+            inclusion = backtrack(nums, target-nums[idx], idx-1);
+        
+        return (inclusion + exclusion);
     }
-    int findTargetSumWays(vector<int>& nums, int target)
+    int findTargetSumWays(vector<int>& nums, int target) 
     {
         int total = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(total+1, -1));
-        return diffEqlToK(nums, total, target, 0, n-1, dp);
+        int newTarget = (total+target)/2;
+        // VV IMP
+        if (total < abs(target) || (target + total) % 2 != 0) return 0;
+
+        return backtrack(nums, newTarget, nums.size()-1);
     }
 };
