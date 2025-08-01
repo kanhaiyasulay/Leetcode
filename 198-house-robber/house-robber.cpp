ca1f34ad25ab61e0dd70aefbcec567d98c72804e
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-08-01 10:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.74%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,7 +15,22 @@ public:
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return backtrack(nums, n-1, dp);
+        if(n == 1) return nums[0];
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2; i<n; i++)
+        {
+            int inclusion = 0;
+            if(i-2 >= 0)
+                inclusion = nums[i] + dp[i-2];
+            int exclusion = dp[i-1];
+
+            dp[i] = max(inclusion, exclusion);
+        }
+
+        return dp.back();
     }
 };
