7bd354398bb34492b353d464d82bc4ed689c8b9c
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-08-06 09:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (21.48%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int mini = 0; // closest sum to target
+
+    void backtrack(vector<int>& stones, int target, int idx, int currentSum, vector<vector<int>>& dp) 
+    {
+        if(currentSum > target) return;
+        if (idx < 0) {
+            if (currentSum <= target)
+            {
+                mini = max(mini, currentSum); // track the best sum ≤ target
+            }
+            return;
+        }
+        if(dp[idx][currentSum] != -1)
+        {
+            mini = max(mini, dp[idx][currentSum]);
+            return;
+        }
+        if(idx >= 0 && currentSum <= target) dp[idx][currentSum] = mini;
+
+        // include current stone
+        backtrack(stones, target, idx - 1, currentSum + stones[idx], dp);
+        // exclude current stone
+        backtrack(stones, target, idx - 1, currentSum, dp);
+    }
+
+    int lastStoneWeightII(vector<int>& stones) {
+        int tot = accumulate(stones.begin(), stones.end(), 0);
+        int target = tot / 2;
+        int n = stones.size();
+
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+
+        backtrack(stones, target, stones.size() - 1, 0, dp);
+        return tot - 2 * mini;
+    }
+};
