901941a07857fd136682bf5b770c00b32ca0940f
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-08-07 12:16:56 +0530

    Time: 39 ms (29.89%) | Memory: 27.6 MB (28.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int backtrack(string& text1, string& text2, int n, int m, vector<vector<int>>& dp)
     {
-        if(n < 0 || m < 0) return 0;
+        if(n == 0 || m == 0) return 0;
 
         if(dp[n][m] != -1) return dp[n][m];
 
-        if(text1[n] == text2[m])
+        if(text1[n-1] == text2[m-1])
             return dp[n][m] = 1 + backtrack(text1, text2, n-1, m-1, dp);
         
         return dp[n][m] = max(backtrack(text1, text2, n-1, m, dp), backtrack(text1, text2, n, m-1, dp));
@@ -14,7 +14,7 @@ public:
     int longestCommonSubsequence(string text1, string text2) 
     {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return backtrack(text1, text2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return backtrack(text1, text2, n, m, dp);
     }
 };
