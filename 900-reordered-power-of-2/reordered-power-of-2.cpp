26762d925f503e5307cb83a3bb6c8b69fdc6edc4
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2025-08-10 23:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (86.43%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+    int generateFreqNum(int n)
+    {
+        int num = 0;
+        while(n > 0)
+        {
+            int digit = n%10;
+            num += pow(10, digit);
+            n /= 10;
+        }
+        return num;
+    }
     bool reorderedPowerOf2(int n) 
     {
-        string curr = to_string(n);
-        sort(curr.begin(), curr.end());
-
+        int freqNum = generateFreqNum(n);
         for(int p=0; p<=29; p++)
         {
-            string s = to_string(1 << p);
-            sort(s.begin(), s.end());
-            if(s == curr) return true;
+            int num = 1 << p;
+            int check = generateFreqNum(num);
+            if(check == freqNum) return true;
         }
+        
         return false;
     }
 };
