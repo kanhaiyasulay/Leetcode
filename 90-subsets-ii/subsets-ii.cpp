e2fd7e50a44eb19e892a47147227bf1ab5e5c3c9
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2026-08-29 23:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (84.73%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void subs(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, int idx)
+    void backtrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, int idx)
     {
         ans.push_back(temp);
 
         for(int i=idx; i<nums.size(); i++)
         {
-            if(i>idx && nums[i] == nums[i-1]) continue;
+            if(i>idx && nums[i]==nums[i-1]) continue;
             
             temp.push_back(nums[i]);
-            subs(nums, ans, temp, i+1);
+            backtrack(nums, ans, temp, i+1);
             temp.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) 
     {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         vector<int> temp;
-        subs(nums, ans, temp, 0);
+        sort(nums.begin(), nums.end());
+
+        backtrack(nums, ans, temp, 0);
         return ans;
     }
 };
