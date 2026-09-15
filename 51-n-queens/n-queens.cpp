0652dd361c34cdd9d2054ba59ca4f7b8f56bff67
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2026-09-15 11:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (93.88%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,49 +1,66 @@
 class Solution {
-    bool isSafe(vector<string>& board, int row, int col, int n)
+public:
+    bool isSafe(vector<string>& tempAns, int row, int col)
     {
-        // Check upper-left diagonal
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+        int n = tempAns.size();
+        for(int i=0; i<col; i++)
+        {
+            if(tempAns[row][i] == 'Q') return false;
+        }
+        // int tempCol = col;
+        // while(tempCol >= 0)
+        // {
+        //     if(tempAns[row][tempCol] == 'Q') return false;
+        //     tempCol--;
+        // }
+
+        int tRow = row, tCol = col;
+        while(tRow >= 0 && tCol >= 0)
+        {
+            if(tempAns[tRow][tCol] == 'Q') return false;
+            tRow--;
+            tCol--;
         }
 
-        // Check lower-left diagonal
-        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
-            if (board[i][j] == 'Q') return false;
+        tRow = row;
+        tCol = col;
+        while(tRow < n && tCol >= 0)
+        {
+            if(tempAns[tRow][tCol] == 'Q') return false;
+            tRow++;
+            tCol--;
         }
 
         return true;
     }
-public:
-    void nQueen(int n, vector<vector<string>>& ans, vector<string>& temp, int row, int col, vector<bool>& rowCheck)
+    void backtrack(vector<vector<string>>& ans, vector<string>& tempAns, int col)
     {
-        if(col >= n)
+        int n = tempAns.size();
+        if(col >= n) 
         {
-            ans.push_back(temp);
+            ans.push_back(tempAns);
             return;
         }
 
-        for(int r=0; r<n; r++)
+        for(int i=0; i<n; i++)
         {
-            if(rowCheck[r]) continue;
-
-            if(!isSafe(temp, r, col, n)) continue;
-
-            temp[r][col] = 'Q';
-            rowCheck[r] = true;
-
-            nQueen(n, ans, temp, r, col+1, rowCheck);
-
-            temp[r][col] = '.';
-            rowCheck[r] = false;
+            if(isSafe(tempAns, i, col))
+            {
+                tempAns[i][col] = 'Q';
+                backtrack(ans, tempAns, col+1);
+                tempAns[i][col] = '.';
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) 
     {
         vector<vector<string>> ans;
-        vector<string> temp(n, string(n, '.'));
-        vector<bool> rowCheck(n, false);
+        vector<string> tempAns;
+        string temp(n, '.');
+        for(int i=0; i<n; i++) tempAns.push_back(temp);
 
-        nQueen(n, ans, temp, 0, 0, rowCheck);
+        backtrack(ans, tempAns, 0);
+
         return ans;
     }
 };
