0b5804dab3814511a8ceb0904c996752f42776b7
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2026-09-19 18:50:41 +0530

    Time: 44 ms (56.41%) | Memory: 57.7 MB (50.07%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,40 +1,38 @@
 class Solution {
-    bool isPalindrome(string s)
+public:
+    bool isPalindrome(string str)
     {
-        int i = 0, j = s.length()-1;
-        while(i < j)
+        int l = 0, r = str.length()-1;
+        while(l < r)
         {
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
+            if(str[l] != str[r]) return false;
+            l++;
+            r--;
         }
         return true;
     }
-public:
-    void backtrack(string s, vector<string>& temp, vector<vector<string>>& ans, int idx)
+    void backtrack(vector<vector<string>>& ans, vector<string>& temp, string s, int idx)
     {
         if(idx == s.length()) 
         {
             ans.push_back(temp);
             return;
         }
-
         for(int i=idx; i<s.length(); i++)
         {
-            string curr = s.substr(idx, i-idx+1);
-            if(isPalindrome(curr))
-            {
-                temp.push_back(curr);
-                backtrack(s, temp, ans, i+1);
-                temp.pop_back();
-            }
+            string str = s.substr(idx, i-idx+1);
+            if(!isPalindrome(str)) continue;
+            temp.push_back(str);
+            backtrack(ans, temp, s, i+1);
+            temp.pop_back();
         }
     }
     vector<vector<string>> partition(string s) 
     {
         vector<vector<string>> ans;
         vector<string> temp;
-        backtrack(s, temp, ans, 0);
+
+        backtrack(ans, temp, s, 0);
         return ans;
     }
 };
