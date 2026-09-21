2c16e62d496ee36b2c09cbea25c316e3ba80c84d
kanhaiyasulay <112163215+kanhaiyasulay@users.noreply.github.com>
2026-09-22 00:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    int backtrack(int n, vector<int>& dp)
+    {
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp.back();
+    }
     int climbStairs(int n) 
     {
-        if(n <= 2) return n;
-        int prev = 2, prev2 = 1;
-        for(int i=3; i<=n; i++)
-        {
-            int curr = prev + prev2;
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        vector<int> dp(n+1, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+        return backtrack(n, dp);    
     }
 };
